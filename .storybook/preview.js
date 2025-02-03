5ce2bae5d2498fd05202cd74ebfd0684d4f42530
abbie <supermercadoabbie@gmail.com>
2025-02-03 21:02:13 +0800

    Chapter 4: Screen

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,14 @@
+import { setup } from '@storybook/vue3'
+
+import { createPinia } from 'pinia'
+
 import '../src/index.css'
 
-//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+//👇 Registers a global Pinia instance inside Storybook to be consumed by existing stories
+setup((app) => {
+  app.use(createPinia())
+})
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
